c9a888d23eca56f822be205d9bab672ecf7cd3b8
Arturo Saldivar <damonbalam@gmail.com>
2023-02-06 15:18:14 -0500

    reto dos

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import {ref,computed } from 'vue'
+import { ref, computed } from 'vue'
 
 const name = "Arturo Saldivar";
 const counter = ref(0);
-
+const listaNumeros = ref([])
 // método
 const increment = () => {
   counter.value++;
@@ -16,27 +16,48 @@ const decrement = () => {
   counter.value--;
 }
 
-const classCounter = computed( ()=> {
+const add = () => {
+  listaNumeros.value.push(counter.value);
+}
 
+const classCounter = computed(() => {
   if (counter.value === 0) {
     return 'color:peru'
   }
-
   return counter.value > 0 ? 'color:green' : 'color:red'
+})
+
+const validateInput = computed(() => {
+  
+  const result = listaNumeros.value.filter( item => item === counter.value );
+
+  return result.length === 0 ? true : false;
 })
 
 </script>
 
 <template>
-<h1>Hola {{ name.toUpperCase() }}</h1>
-<h2 :style="classCounter">{{ counter }}</h2>
-<button @click="increment">Aumentar</button>
-<button @click="reset">Reset</button>
-<button @click="decrement">Disminuir</button>
+  <div class="container mx-auto d-flex flex-column justify-content-center align-items-center mt-5">
+    <h1>Hola {{ name }}</h1>
+    <h2 :style="classCounter" class="fs-1">{{ counter }}</h2>
+    <div>
+      <button class="btn btn-success mx-1" @click="increment">Aumentar</button>
+      <button class="btn btn-secondary mx-1" @click="reset">Resetear</button>
+      <button class="btn btn-danger mx-1" @click="decrement">Disminuir</button>
+    </div>
+    <button class="btn btn-primary mt-2 px-5" @click="add" :disabled="!validateInput">Agregar</button>
+    <h2 class="mt-5">Numero favoritos:</h2>
+    <ul class="list-group">
+      <li class="list-group-item text-center" style="width: 200px;" v-for="num in listaNumeros">
+        {{ num }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
-h1{
-  color: red;
-}
+/* *, *:before, *:after {
+	box-sizing: border-box;
+	outline: none;
+} */
 </style>
