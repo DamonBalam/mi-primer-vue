f6cc1c79897ac7f266ed98177765604a68ab986f
Arturo Saldivar <damonbalam@gmail.com>
2023-02-06 14:01:25 -0500

    Implementación y uso de ref

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <script setup>
-const name = "Arturo Saldivar";
+import {ref } from 'vue'
 
+const name = "Arturo Saldivar";
+const counter = ref(0);
 
 // método
-const handleClick = (message) => {
-  console.log(message);
+const increment = () => {
+  console.log('aumentar contador');
+  counter.value++;
 }
 
 </script>
 
 <template>
 <h1>Hola {{ name.toUpperCase() }}</h1>
-<button @click.right="handleClick('Texto Right')">Activame right</button>
-<button @click.middle="handleClick('Texto Middle')">Activame middle</button>
-<button @click="handleClick('Texto Left')">Activame left</button>
+<h2>{{ counter }}</h2>
+<button @click="increment">Aumentar</button>
 </template>
 
 <style scoped>
